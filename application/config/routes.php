728c0afc72a39fbb4859787c621e2d2cc07ec68a
vienbk91 <vienbk91@gmail.com>
2016-08-09 20:48:14 +0900

    change routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+//$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'mypage_con';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+/*
+ | -------------------------------------------------------------------------
+ | STOP SERVICE ROUTING
+ | -------------------------------------------------------------------------
+ |	config stop service start
+ */
+//$route[':any'] = 'stopservice_con';
+//$route['default_controller'] = 'stopservice_con';
